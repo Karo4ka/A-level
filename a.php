38231e2f0283f266ed4e05e2ae4999dd50bff55d
Karo4ka <karina.marochkina@icloud.com>
2018-11-27 16:59:34 +0200

    lalala

--- a.php
+++ a.php
@@ -8,4 +8,26 @@ pritected function __construct($name, $lastname, $surname, $age)
     $this->fullName = $surname;
     $this->fullName = $age;
 }
+    public function getName()
+    {
+        return $this->Name;
+    }
+
+    public function getLastname()
+    {
+        return $this->Lastname;
+    }
+
+    public function setName($name)
+    {
+        if (gettype($name) === 'string') {
+            $this->fullName = $name;
+        }
+    }
+
+    public function setLastname(array $lastname)
+    {
+        $this->Lastname = $lastname;
+    }
+
 }
