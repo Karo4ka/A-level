272a39388c46d526e4639ecd8d5495e6feb487b4
mbkkong <40576246+mbkkong@users.noreply.github.com>
2018-11-29 19:30:47 +0200

    Update a.php

--- a.php
+++ a.php
@@ -1,56 +1,67 @@
 <?php
-class Students {
-    public  $name, $surname, $lastname, $age;
-public function __construct($name, $lastname, $surname, $age)
+
+class Students
 {
-    $this->fullName = $name;
-    $this->fullName = $lastname;
-    $this->fullName = $surname;
-    $this->fullName = $age;
-}
-    public function getName(){
+    public $name, $surname, $lastname, $age;
+
+    public function __construct($name, $lastname, $surname, $age)
+    {
+        $this->fullName = $name;
+        $this->fullName = $lastname;
+        $this->fullName = $surname;
+        $this->fullName = $age;
+    }
+
+    public function getName()
+    {
         return $this->Name;
     }
 
-    public function getLastname(){
+    public function getLastname()
+    {
         return $this->Lastname;
     }
 
-    public function getSurname(){
-    return $this->Surname;
+    public function getSurname()
+    {
+        return $this->Surname;
     }
 
-    public function getAge(){
+    public function getAge()
+    {
         return $this->Age;
     }
 
-    public function setName($name){
+    public function setName($name)
+    {
         if (gettype($name) === 'string') {
             $this->fullName = $name;
         }
     }
 
-    public function setLastname(array $lastname){
+    public function setLastname(array $lastname)
+    {
         if (gettype($lastname) === 'string') {
-        $this->Lastname = $lastname;
+            $this->Lastname = $lastname;
         }
     }
 
-    public function setSurname($surname){
-        if (gettype($surname) === 'string'){
-            $this->Surname=$surname;
+    public function setSurname($surname)
+    {
+        if (gettype($surname) === 'string') {
+            $this->Surname = $surname;
         }
 
     }
 
 }
-$student = new Students();
+
+$student       = new Students();
 $student->Name = 'Karina';
 
-    public function setAge(array $age)
-    {
-        $this->accessRights = $age;
-    }
 
+function setAge(array $age)
+{
+    $this->accessRights = $age;
 }
 
